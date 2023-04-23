7c2018ce2712d51b8d0736441cb5b27f84e587cd
pranali1009 <pranalipahurkar@gmail.com>
2023-04-23 23:16:30 +0530

    base method for the operations

--- index.c
+++ index.c
@@ -1,14 +1,48 @@
 #include<stdio.h>
-void main()
-{
 
-printf("1) Add new password\n");
-printf("2) Get password\n");
-printf("3) Update password\n");
-printf("4) Delete password\n");
+void addPassword() {
+    printf("Add new password");
+}
 
-int selectedOperation;
-scanf("%d", &selectedOperation);
+void getPassword() {
+    printf("Get password");
+}
 
-printf("\nUser selected the operation: %d", selectedOperation);
+void updatePassword() {
+    printf("Update password");
+}
+
+void deletePassword() {
+    printf("Delete password");
+}
+
+void main() {
+
+    // display all operations
+    printf("1) Add new password\n");
+    printf("2) Get password\n");
+    printf("3) Update password\n");
+    printf("4) Delete password\n");
+    printf("Select operation: ");
+
+    // Save user's option
+    int selectedOperation;
+    scanf("%d", &selectedOperation);
+
+    switch(selectedOperation) {
+        case 1:
+            addPassword();
+            break;
+        case 2:
+            getPassword();
+            break;
+        case 3:
+            updatePassword();
+            break;
+        case 4:
+            deletePassword();
+            break;
+        default:
+            printf("\nIncorrect option selected");
+    }
 }

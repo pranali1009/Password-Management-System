2af3dccb99aa29096f4856e6467bba07bb552631
pranali1009 <pranalipahurkar@gmail.com>
2023-04-26 22:06:52 +0530

    get password

--- index.c
+++ index.c
@@ -5,6 +5,16 @@ int count = 100;
 int charLimit = 100;
 int counter = 0;
 
+void printAllApps(char applications[][charLimit]) {
+    printf("\nAll saved applications:");
+    for (int i = 0 ; i < counter ; i++) {
+        if (strcmp(applications[i], "") != 0) {
+            // if application is not empty string, then it is valid app and print it
+            printf("\n%d) %s", i + 1, applications[i]);
+        }
+    }
+}
+
 void addPassword(char applications[][charLimit], char passwords[][charLimit]) {
     char newPassword[charLimit];
     char newApplication[charLimit];
@@ -17,16 +27,31 @@ void addPassword(char applications[][charLimit], char passwords[][charLimit]) {
     printf("Enter password for %s: ", newApplication);
     scanf("%s", newPassword);
 
-    // update in global apps and passwords lists
+    // update in master apps and passwords lists
     strcpy(applications[counter], newApplication);
     strcpy(passwords[counter], newPassword);
     
-    // update pointer of apps and passwords lists
+    // update pointer of master apps and passwords lists
     counter++;
 }
 
-void getPassword() {
-    printf("Get password");
+void getPassword(char applications[][charLimit], char passwords[][charLimit]) {
+    if (counter == 0) {
+        // since no app was saved, we cant get any password
+        
+        printf("No app has been saved\n");
+        return;
+    }
+
+    printAllApps(applications);
+    
+    // temp store app id
+    int appId;
+    printf("\nEnter id of app: ");
+    scanf("%d", &appId);
+
+    // get password from master passwords lists for the saved appId
+    printf("Password of app %s is %s\n", applications[appId - 1], passwords[appId - 1]);
 }
 
 void updatePassword() {
@@ -40,32 +65,39 @@ void deletePassword() {
 void main() {
     char applications[count][charLimit];
     char passwords[count][charLimit];
-
-    // display all operations
-    printf("1) Add new password\n");
-    printf("2) Get password\n");
-    printf("3) Update password\n");
-    printf("4) Delete password\n");
-    printf("Select operation: ");
-
-    // Save user's option
     int selectedOperation;
-    scanf("%d", &selectedOperation);
 
-    switch(selectedOperation) {
-        case 1:
-            addPassword(applications, passwords);
-            break;
-        case 2:
-            getPassword();
-            break;
-        case 3:
-            updatePassword();
-            break;
-        case 4:
-            deletePassword();
-            break;
-        default:
-            printf("\nIncorrect option selected");
+    while(1) {
+        // display all operations
+        printf("\n1) Add new password\n");
+        printf("2) Get password\n");
+        printf("3) Update password\n");
+        printf("4) Delete password\n");
+        printf("5) Quit\n");
+        printf("Select operation: ");
+
+        // Save user's option
+        scanf("%d", &selectedOperation);
+
+        switch(selectedOperation) {
+            case 1:
+                addPassword(applications, passwords);
+                break;
+            case 2:
+                getPassword(applications, passwords);
+                break;
+            case 3:
+                updatePassword();
+                break;
+            case 4:
+                deletePassword();
+                break;
+            case 5:
+            printf("Bye :(");
+                return;
+            default:
+                printf("\nIncorrect option selected");
+        }
     }
+
 }
